b7b0c4948bd8ec0a44cdf6dc658f9bb86253b419
Hari Vishwanath <mailtohari134@gmail.com>
2024-08-02 14:49:21 +0530

    Create coderun.is-an.app.js

---
+++ domains/coderun.is-an.app.js
@@ -0,0 +1,10 @@
+addSubDomain({
+  description: 'Website for my code judge platform',
+  domain: 'is-an.app',
+  subdomain: 'coderun',
+  owner: { email: 'mailtohari134@gmail.com', },
+  record: {
+    A: ['142.93.218.83',],
+  },
+  proxy: false,
+})
